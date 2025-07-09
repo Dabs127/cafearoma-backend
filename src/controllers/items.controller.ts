a8fa76e3ae3680e0bda5dc08f64f988a164e920e
Daniel <danielSerna127@outlook.com>
2025-07-09 11:17:22 -0600

    feat: Add Update and Delete functionality

--- src/controllers/items.controller.ts
+++ src/controllers/items.controller.ts
@@ -1,9 +1,9 @@
 // import { AuthenticatedRequest } from "#types/AuthenticatedRequest.js";
 import { Item } from "../models/Item.js";
 import { Request, RequestHandler, Response } from "express";
+import { CreateItemDto, DeleteIdItem, UpdateItemDto } from "#types/Items.js";
+import { uploadImage } from "#utils/uploadToCloudinary.js";
 import cloudinary from "#config/cloudinary.js";
-import { UploadApiResponse } from "cloudinary";
-import { CreateItemDto } from "#types/Items.js";
 
 export const getItems: RequestHandler = async (req: Request, res: Response) => {
   try {
@@ -15,6 +15,21 @@ export const getItems: RequestHandler = async (req: Request, res: Response) => {
   } catch (error) {
     console.error("Error fetching items:", error);
     res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getItemById: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    res.status(200).json({
+      item,
+    });
+  } catch (error) {
+    console.error("Erro getting item:", error);
+    res.status(500).json({ message: "Error Devolviendo la info del item" });
   }
 };
 
@@ -30,7 +45,7 @@ export const postItem: RequestHandler = async (
       throw Error();
     }
 
-    const uploadResult = await uploadItemImage(fileBuffer);
+    const uploadResult = await uploadImage(fileBuffer, "cafe-aroma/menu-items");
     const urlImage = uploadResult.secure_url;
     const newItem = new Item({
       category: req.body.category,
@@ -54,35 +69,124 @@ export const postItem: RequestHandler = async (
   return;
 };
 
-export const uploadItemImage = async (
-  fileBuffer: Buffer
-): Promise<UploadApiResponse> => {
-  return await new Promise((resolve, reject) => {
-    cloudinary.uploader
-      .upload_stream(
-        {
-          transformation: [
-            {
-              quality: "auto",
-              fetch_format: "auto",
-            },
-            {
-              width: 1200,
-              height: 1200,
-              crop: "fill",
-              gravity: "auto",
-            },
-          ],
-          folder: "cafe-aroma/menu-items",
-        },
-        (error, uploadResult) => {
-          if (error) {
-            reject(new Error(error.message.toString()));
-          }
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          resolve(uploadResult!);
-        }
-      )
-      .end(fileBuffer);
-  });
+export const deleteItem: RequestHandler = async (
+  req: Request<unknown, unknown, DeleteIdItem>,
+  res: Response
+) => {
+  try {
+    const { id } = req.body;
+
+    if (!id) throw new ReferenceError("Id was not given");
+
+    const result = await Item.findById(id);
+
+    if (!result) {
+      res.status(404).json({
+        message: `Item with id ${id.toString()} not found`,
+      });
+      return;
+    }
+
+    let substrings = result.imgUrl.split("upload");
+
+    substrings = substrings[1].split("/");
+    substrings.splice(0, 2);
+    const finalString = substrings.join("/").split(".")[0];
+
+    console.log(finalString);
+
+    await cloudinary.uploader.destroy(finalString, (result) => {
+      console.log(result);
+    });
+
+    await Item.deleteOne({ _id: id });
+
+    console.log("Eliminado con exito");
+
+    res.status(200).json({
+      success: true,
+      message: "Item eliminado exitosamente",
+    });
+  } catch (err) {
+    console.error(err);
+
+    res.status(400).json({
+      success: false,
+      message: "Fallo el eliminar el item del menú",
+    });
+  }
+};
+
+export const updateItem: RequestHandler = async (
+  req: Request<unknown, unknown, UpdateItemDto>,
+  res: Response
+) => {
+  try {
+    console.log("Cookies en postItem:", req.cookies);
+    console.log("Content-Type:", req.headers["content-type"]);
+    console.log(req.body);
+    const fileBuffer = req.file?.buffer;
+    const { id } = req.body;
+
+    if (!id) throw new ReferenceError("Id was not given");
+
+    let urlImage: string | undefined;
+
+    if (fileBuffer) {
+      const result = await Item.findById(id);
+
+      if (!result) {
+        res.status(404).json({
+          message: `Item with id ${id.toString()} not found`,
+        });
+        return;
+      }
+
+      let substrings = result.imgUrl.split("upload");
+
+      substrings = substrings[1].split("/");
+      substrings.splice(0, 2);
+      const finalString = substrings.join("/").split(".")[0];
+
+      console.log(finalString);
+
+      await cloudinary.uploader.destroy(finalString, (result) => {
+        console.log(result);
+      });
+
+      const uploadResult = await uploadImage(
+        fileBuffer,
+        "cafe-aroma/menu-items"
+      );
+      urlImage = uploadResult.secure_url;
+    }
+
+    const updates: Partial<UpdateItemDto> = {};
+    const fields = ["category", "description", "name", "price"] as const;
+
+    // Solo actualiza los campos que fueron enviados
+    for (const field of fields) {
+      if (Object.prototype.hasOwnProperty.call(req.body, field)) {
+        updates[field] = req.body[field];
+      }
+    }
+
+    // Solo si hay imagen nueva
+    if (urlImage) {
+      updates.imgUrl = urlImage;
+    }
+
+    await Item.findByIdAndUpdate(id, updates);
+
+    res.status(201).json({
+      message: "Item actualizado exitosamente",
+    });
+    return;
+  } catch (error) {
+    console.error("Error creating item:", error);
+    res
+      .status(400)
+      .json({ message: "Something gone wrong while updating the item" });
+  }
+  return;
 };
--- src/types/Items.ts
+++ src/types/Items.ts
@@ -1,7 +1,19 @@
 export interface CreateItemDto {
   category: string;
   description: string;
-  imgUrl: string;
   name: string;
   price: number;
+}
+
+export interface UpdateItemDto {
+  id: number;
+  category: string;
+  description: string;
+  imgUrl: string;
+  name: string;
+  price: string;
+}
+
+export interface DeleteIdItem {
+  id: number;
 }
